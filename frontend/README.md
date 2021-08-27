7369e69691edc93550aad62bb377ca89e41f4587
yashkasera <yashkasera@icloud.com>
2021-08-27 18:16:26 +0530

    Initial Commit

---
+++ frontend/README.md
@@ -0,0 +1,1 @@
+# MidPay_Frontend
---
+++ frontend/sites/seller/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          MidPay Seller Portal
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
